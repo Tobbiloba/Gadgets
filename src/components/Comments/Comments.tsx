2dd05af0474ae618e5d26535427e7afe55729646
Tobbiloba <salauoluwatobiloba0@gmail.com>
2023-01-03 20:46:21 +0100

    third slider

--- src/components/Comments/Comments.tsx
+++ src/components/Comments/Comments.tsx
@@ -12,6 +12,8 @@ import { Autoplay, Pagination, Navigation } from 'swiper'
 // import images
 import man1 from './people/man1.jpg'
 import female1 from './people/female1.webp'
+import man2 from './people/man2.jpg'
+import female2 from './people/female2.webp'
 
 export const Comments = () => {
   return (
@@ -46,7 +48,7 @@ export const Comments = () => {
             </p>
           </div>
         </SwiperSlide> */}
-        <SwiperSlide className="slider2">
+        {/* <SwiperSlide className="slider2">
           <div className="comment2__text">
             <h2>CHRIS HEMSWORTH</h2>
             <h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</h3>
@@ -61,9 +63,24 @@ export const Comments = () => {
             </p>
           </div>
           <img src={female1} className="comment2__img" alt="img1" />
+        </SwiperSlide> */}
+        <SwiperSlide className="slider1">
+          <img src={man2} className="comment1__img" alt="img1" />
+          <div className="comment1__text">
+            <h2>CHRIS HEMSWORTH</h2>
+            <h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</h3>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat
+              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+              sunt in culpa qui officia deserunt mollit anim id est laborum.{' '}
+            </p>
+          </div>
         </SwiperSlide>
-        {/* <SwiperSlide>Slide 3</SwiperSlide>
-        <SwiperSlide>Slide 4</SwiperSlide>
+        {/* <SwiperSlide>Slide 4</SwiperSlide>
         <SwiperSlide>Slide 5</SwiperSlide>
         <SwiperSlide>Slide 6</SwiperSlide>
         <SwiperSlide>Slide 7</SwiperSlide>
