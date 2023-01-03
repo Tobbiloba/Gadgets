0895396c05da12e8cb2500704c8b0d37dcd432fa
Tobbiloba <salauoluwatobiloba0@gmail.com>
2023-01-03 20:43:59 +0100

    added sceond comment slider

--- src/components/Comments/Comments.tsx
+++ src/components/Comments/Comments.tsx
@@ -9,6 +9,10 @@ import 'swiper/css/navigation'
 // import required modules
 import { Autoplay, Pagination, Navigation } from 'swiper'
 
+// import images
+import man1 from './people/man1.jpg'
+import female1 from './people/female1.webp'
+
 export const Comments = () => {
   return (
     <div className="comments__container">
@@ -26,9 +30,39 @@ export const Comments = () => {
         modules={[Autoplay, Pagination, Navigation]}
         className="mySwiper"
       >
-        <SwiperSlide className="slider1">Slide 1</SwiperSlide>
-        {/* <SwiperSlide>Slide 2</SwiperSlide>
-        <SwiperSlide>Slide 3</SwiperSlide>
+        {/* <SwiperSlide className="slider1">
+          <img src={man1} className="comment1__img" alt="img1" />
+          <div className="comment1__text">
+            <h2>CHRIS HEMSWORTH</h2>
+            <h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</h3>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat
+              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+              sunt in culpa qui officia deserunt mollit anim id est laborum.{' '}
+            </p>
+          </div>
+        </SwiperSlide> */}
+        <SwiperSlide className="slider2">
+          <div className="comment2__text">
+            <h2>CHRIS HEMSWORTH</h2>
+            <h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</h3>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat
+              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+              sunt in culpa qui officia deserunt mollit anim id est laborum.{' '}
+            </p>
+          </div>
+          <img src={female1} className="comment2__img" alt="img1" />
+        </SwiperSlide>
+        {/* <SwiperSlide>Slide 3</SwiperSlide>
         <SwiperSlide>Slide 4</SwiperSlide>
         <SwiperSlide>Slide 5</SwiperSlide>
         <SwiperSlide>Slide 6</SwiperSlide>
