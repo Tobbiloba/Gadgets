b287ba1d28a6b6a6447a666223dfc7943a7d1713
Tobbiloba <salauoluwatobiloba0@gmail.com>
2023-01-05 21:17:28 +0100

    added more products

--- src/components/Sections/Mobile Devices/Infinix/Infinix.tsx
+++ src/components/Sections/Mobile Devices/Infinix/Infinix.tsx
@@ -1,5 +1,12 @@
 //import required images
-import infinixNote12Pro from './infinix/note12_pro5G.jpg'
+import Note12Pro from './infinix/note12_pro5G.jpg'
+import Zero5g from './infinix/zero5g.jpg'
+import zeroxpro from './infinix/zeroxpro.jpg'
+import zerox from './infinix/zerox.jpg'
+import note12vip from './infinix/note12vip.png'
+import zeroultra from './infinix/ZEROULTRA_List.png'
+import note12i from './infinix/note12i.png'
+import hot20i from './infinix/hot20i.png'
 import './Infinix.css'
 export const Infinix = () => {
   return (
@@ -9,7 +16,7 @@ export const Infinix = () => {
       </div>
       <div className="infinix__container-scroll">
         <div className="infinix__container-products">
-          <img src={infinixNote12Pro} alt="products" />
+          <img src={Note12Pro} alt="products" className="whitebg" />
           <h2>Infinix Note 12 PRO</h2>
           <p>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo
@@ -19,7 +26,17 @@ export const Infinix = () => {
           </p>
         </div>
         <div className="infinix__container-products">
-          <img src={infinixNote12Pro} alt="products" />
+          <img src={Zero5g} alt="products" className="whitebg" />
+          <h2>Infinix Zero 5</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo
+            accusantium eveniet numquam fuga cum quidem facere eaque autem,
+            necessitatibus placeat optio ducimus eligendi quisquam? Aliquam
+            labore officiis ex obcaecati nesciunt!
+          </p>
+        </div>
+        <div className="infinix__container-products">
+          <img src={zerox} alt="products" className="whitebg" />
           <h2>Infinix Note 12 PRO</h2>
           <p>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo
@@ -29,7 +46,7 @@ export const Infinix = () => {
           </p>
         </div>
         <div className="infinix__container-products">
-          <img src={infinixNote12Pro} alt="products" />
+          <img src={zeroxpro} alt="products" className="whitebg" />
           <h2>Infinix Note 12 PRO</h2>
           <p>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo
@@ -39,7 +56,7 @@ export const Infinix = () => {
           </p>
         </div>
         <div className="infinix__container-products">
-          <img src={infinixNote12Pro} alt="products" />
+          <img src={note12vip} alt="products" />
           <h2>Infinix Note 12 PRO</h2>
           <p>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo
@@ -49,7 +66,7 @@ export const Infinix = () => {
           </p>
         </div>
         <div className="infinix__container-products">
-          <img src={infinixNote12Pro} alt="products" />
+          <img src={zeroultra} alt="products" />
           <h2>Infinix Note 12 PRO</h2>
           <p>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo
@@ -59,7 +76,17 @@ export const Infinix = () => {
           </p>
         </div>
         <div className="infinix__container-products">
-          <img src={infinixNote12Pro} alt="products" />
+          <img src={note12i} alt="products" />
+          <h2>Infinix Note 12 PRO</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo
+            accusantium eveniet numquam fuga cum quidem facere eaque autem,
+            necessitatibus placeat optio ducimus eligendi quisquam? Aliquam
+            labore officiis ex obcaecati nesciunt!
+          </p>
+        </div>
+        <div className="infinix__container-products">
+          <img src={hot20i} alt="products" />
           <h2>Infinix Note 12 PRO</h2>
           <p>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo
